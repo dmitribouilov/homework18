8a9f0dfdb94e5ed17a247eb7706b137d54766e25
dmitribouilov <59586265+dmitribouilov@users.noreply.github.com>
2020-07-08 14:40:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,15 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect( "mongodb+srv://dimonya:eblan1@dmitricluster-ke366.mongodb.net/budget?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false});
 
 
 //mongoose.connect("mongodb://localhost/budget", {
  // useNewUrlParser: true,
  // useFindAndModify: false
 
- // mongodb+srv://dimonya:eblan1@dmitricluster-ke366.mongodb.net/budget
-//});
 
 // routes
 app.use(require("./routes/api.js"));
